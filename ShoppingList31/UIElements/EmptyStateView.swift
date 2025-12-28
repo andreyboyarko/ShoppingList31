11b662733ab848f8cc7e6be74c138c64157ac42d
Brabus098 <vovan1998888@mail.ru>
2025-12-28 20:40:27 +0300

    [Feature] Create EmptyStateView in UIElements

---
+++ ShoppingList31/UIElements/EmptyStateView.swift
@@ -0,0 +1,33 @@
+//
+//  EmptyStateView.swift
+//  ShoppingList31
+//
+//  Created by Владимир on 28.12.2025.
+//
+
+import SwiftUI
+
+struct EmptyStateView: View {
+    
+    // MARK: - Property
+    
+    let viewState: EmptyStateType
+    
+    // MARK: - Body
+    
+    var body: some View {
+        VStack(spacing: 10) {
+            Image(viewState.imageName)
+                .resizable()
+                .scaledToFit()
+                .padding(.bottom)
+            
+            Text(EmptyStateType.title)
+                .font(.mediumTitle.bold())
+            
+            Text(viewState.subtitle)
+                .font(.body)
+        }
+        .padding(16)
+    }
+}
