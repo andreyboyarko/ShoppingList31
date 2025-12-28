17644fac65b7d91cf6ad9f6e02024e71f0f04707
Brabus098 <vovan1998888@mail.ru>
2025-12-28 20:39:59 +0300

    [Feature] Add EmptyStateType enum to Models

---
+++ ShoppingList31/Models/EmptyStateType.swift
@@ -0,0 +1,39 @@
+//
+//  EmptyStateType.swift
+//  ShoppingList31
+//
+//  Created by Владимир on 28.12.2025.
+//
+
+/// Перечисление состояний пустого экрана в приложении.
+/// Используется для отображения соответствующих сообщений и изображений
+/// когда в интерфейсе нет данных для показа.
+///
+/// При создании EmptyStateView выбирай:
+/// createShoppingList если нужна заглушка для экран - "Главный экран - не создан список"
+/// addItemToShoppingList если нужна заглушка для экрана - "Перечень товаров"
+
+enum EmptyStateType {
+    case createShoppingList
+    case addItemToShoppingList
+    
+    static let title = "Давайте спланируем покупки!"
+    
+    var subtitle: String {
+        switch self {
+        case .createShoppingList:
+            "Создайте свой первый список"
+        case .addItemToShoppingList:
+            "Начните добавлять товары"
+        }
+    }
+    
+    var imageName: String {
+        switch self {
+        case .createShoppingList:
+            "work"
+        case .addItemToShoppingList:
+            "shopping"
+        }
+    }
+}
