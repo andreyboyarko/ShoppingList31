a274dec69a67feeef8143279a7f78666cf53d43e
Sun4chek <boysfromhell@mail.ru>
2026-01-14 23:48:37 +0300

    Add Main screen

---
+++ ShoppingList31/Screens/MainScreen/MainScreen.swift
@@ -0,0 +1,18 @@
+//
+//  MainScreen.swift
+//  ShoppingList31
+//
+//  Created by Волошин Александр on 1/13/26.
+//
+
+import SwiftUI
+
+struct MainScreen: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    MainScreen()
+}
