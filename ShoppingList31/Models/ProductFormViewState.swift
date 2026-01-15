9570f59accf61a646f8edbfca8e6572100d593b3
Brabus098 <vovan1998888@mail.ru>
2026-01-15 17:27:06 +0300

    [Feature] Implement state management for ProductFormView

---
+++ ShoppingList31/Models/ProductFormViewState.swift
@@ -0,0 +1,13 @@
+//
+//  ProductFormViewState.swift
+//  ShoppingList31
+//
+//  Created by Владимир on 14.01.2026.
+//
+
+import SwiftUI
+
+enum ProductFormViewState {
+    case creating
+    case editing
+}
